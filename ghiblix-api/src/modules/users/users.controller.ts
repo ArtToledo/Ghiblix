e13d6913506a6462aed584290d72c3a6cef70270
Nayla Gomes <nayla.gomes@squadra.com.br>
2020-12-22 13:25:42 -0300

    controller e service

--- ghiblix-api/src/modules/users/users.controller.ts
+++ ghiblix-api/src/modules/users/users.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
-  constructor() {}
+  constructor(
+    private usersService: UsersService
+  ) {}
+
+  @Get()
+  async getAll() {
+    return await this.usersService.findAll();
+  }
+
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return this.usersService.create(user);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() user: User): Promise<User> {
+    user.id = id;
+    return this.usersService.update(id, user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    this.usersService.delete(id);
+  }
 }
--- ghiblix-api/src/modules/users/users.service.ts
+++ ghiblix-api/src/modules/users/users.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(
+        @Inject('USER_REPOSITORY') private usersRepository: typeof User
+    ){}
+
+    findAll(): Promise<User[]> {
+        return this.usersRepository.findAll<User>();
+    }
+    
+    getById(id: number): Promise<User> {
+        return this.usersRepository.findByPk(id);
+    }
+    
+    create(user: User): Promise<User> {
+        return this.usersRepository.create(user);
+    }
+
+    update(id:number, user: User): Promise<User> {
+        this.usersRepository.update({ user }, { where: {id: id} });
+        return this.getById(id);
+    }
+
+    async delete(id: number) {
+
+        this.usersRepository.destroy({where: {id: id}});
+    }
+}
