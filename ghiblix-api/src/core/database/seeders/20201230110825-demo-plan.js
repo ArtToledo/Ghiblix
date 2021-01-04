5ec1b40cb8437ba62dbd5ae06da36d44f117c500
Tarcila Fernanda Resende da Silva <tarcila.fernanda@squadra.com.br>
2021-01-04 12:48:22 -0300

    Alterando tipagem da propriedade 'price' da entidade Plan

--- ghiblix-api/src/core/database/seeders/20201230110825-demo-plan.js
+++ ghiblix-api/src/core/database/seeders/20201230110825-demo-plan.js
@@ -4,17 +4,17 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('plans', [{
       name: 'Básico',
-      price: '21',
+      price: '21.9',
       profiles_count: '1'
     },
     {
       name: 'Padrão',
-      price: '32',
+      price: '32.9',
       profiles_count: '2'
     },
     {
       name: 'Premium',
-      price: '45',
+      price: '45.9',
       profiles_count: '4'
     }
   ]);
