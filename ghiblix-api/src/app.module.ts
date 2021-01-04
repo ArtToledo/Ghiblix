d7d076375883dfb7005613e17907950c954ebd03
ArtToledo <arthur.toledo741@gmail.com>
2021-01-04 14:35:02 -0300

    Integracao da criacao do usuario com envio de e-mail boas vindas

--- ghiblix-api/src/app.module.ts
+++ ghiblix-api/src/app.module.ts
@@ -4,21 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './core/auth/auth.module';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
-<<<<<<< HEAD
 import { EmailModule } from './shared/email/email.module';
-=======
->>>>>>> developer
 
 @Module({
   imports: [
     DatabaseModule,
-<<<<<<< HEAD
     EmailModule,
-    UsersModule
-=======
     UsersModule,
     AuthModule
->>>>>>> developer
   ],
   controllers: [AppController],
   providers: [AppService],
--- ghiblix-api/src/shared/email/email.service.ts
+++ ghiblix-api/src/shared/email/email.service.ts
@@ -5,17 +5,14 @@ import { MailerService } from '@nestjs-modules/mailer';
 export class EmailService {
   constructor(private readonly mailerService: MailerService) {}
   
-  public sendEmailTest(username: string): any {
+  public sendEmailTest(emailUser: string): any {
     this
       .mailerService
       .sendMail({
-        to: 'arthur.toledo741@gmail.com',
-        from: 'lojageekmanias@gmail.com',
+        to: emailUser,
+        from: 'ghiblixfilmes@gmail.com',
         subject: 'Bem vindo a Ghiblix',
         template: 'welcome',
-        context: {
-          username,
-        },
       })
       .then((success) => {})
       .catch((err) => {
